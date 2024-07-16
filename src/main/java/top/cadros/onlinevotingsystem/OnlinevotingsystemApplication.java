64f1331e7a0af21ba2a26a680e0b14a4e85c1292
cadros1 <cadros1@outlook.com>
2024-07-16 14:32:36 +0800

    初始化项目

---
+++ src/main/java/top/cadros/onlinevotingsystem/OnlinevotingsystemApplication.java
@@ -0,0 +1,13 @@
+package top.cadros.onlinevotingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinevotingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinevotingsystemApplication.class, args);
+	}
+
+}
