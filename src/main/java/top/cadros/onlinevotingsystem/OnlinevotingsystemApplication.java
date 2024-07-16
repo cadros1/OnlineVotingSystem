ee4ceaeb8e598b08cba9760c806d12064829adfd
cadros1 <cadros1@outlook.com>
2024-07-16 16:23:50 +0800

    验证后端部分技术可行性

--- src/main/java/top/cadros/onlinevotingsystem/OnlinevotingsystemApplication.java
+++ src/main/java/top/cadros/onlinevotingsystem/OnlinevotingsystemApplication.java
@@ -2,12 +2,11 @@ package top.cadros.onlinevotingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class OnlinevotingsystemApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(OnlinevotingsystemApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(OnlinevotingsystemApplication.class, args);
 	}
-
 }
