4237d4914b0ed02d7370bde011c215ed715b91bd
cadros1 <cadros1@outlook.com>
2024-07-18 10:33:36 +0800

    添加User类及其数据库查询操作

---
+++ backend/src/main/java/top/cadros/onlinevotingsystem/object/User.java
@@ -0,0 +1,25 @@
+package top.cadros.onlinevotingsystem.object;
+
+public class User {
+    private String account;
+    private String password;
+    private String username;
+
+    public User(String account, String password, String username) {
+        this.account = account;
+        this.password = password;
+        this.username = username;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
