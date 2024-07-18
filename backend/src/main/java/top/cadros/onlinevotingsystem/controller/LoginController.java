a990f45cf8012f0a51670ccf1ea435d72a44d463
cadros1 <cadros1@outlook.com>
2024-07-18 11:13:33 +0800

    将响应封装为ApiReponse

--- backend/src/main/java/top/cadros/onlinevotingsystem/controller/LoginController.java
+++ backend/src/main/java/top/cadros/onlinevotingsystem/controller/LoginController.java
@@ -2,7 +2,7 @@ package top.cadros.onlinevotingsystem.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import top.cadros.onlinevotingsystem.object.User;
+import top.cadros.onlinevotingsystem.object.*;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,16 +13,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class LoginController{
 
     @PostMapping("/login")
-    public ResponseEntity<?> postMethodName(@RequestBody User user) {
+    public ResponseEntity<ApiResponse> postMethodName(@RequestBody User user) {
         try{
             User entity = top.cadros.onlinevotingsystem.service.DataBase.queryUserByAccount(user);
             if(entity != null){
-                return ResponseEntity.ok(entity);
+                return ResponseEntity.ok(new ApiResponse(20000, "登录成功", entity));
             }else{
-                return ResponseEntity.badRequest().body("账号或密码错误");
+                return ResponseEntity.status(401).body(new ApiResponse(40101, "账号或密码不正确", entity));
             }
         }catch(Exception e){
-            return ResponseEntity.status(500).body(e.getMessage());
+            return ResponseEntity.status(500).body(new ApiResponse(50000, e.getMessage(), null));
         }
     }
 }
